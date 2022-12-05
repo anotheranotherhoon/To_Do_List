251d1127c33ed6d6811015e682a8f0530a1315cb
LeeChangHoon <changhoon1030@gmail.com>
2022-12-05 22:36:37 +0900

    ⭐ feat : 로그인, 회원가입, todo API 추가

---
+++ src/api/todo.tsx
@@ -0,0 +1,55 @@
+import { getToken } from '../utils/localeStorage';
+import axiosInstance from './axiosInstance';
+
+interface ITodo {
+  todo : string
+}
+const token = getToken()
+
+export const createTodo = async(todo : ITodo)=> {
+  await axiosInstance.post('/todos', todo,{
+    headers : {
+      Authorization : `Bearer ${token}`
+    }
+  }
+  )
+};
+
+export const getTodos = async () => {
+  const response =  await axiosInstance.get('/todos', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  const todos = response.data
+  return todos
+};
+
+export const updateTodo = async (id:number, todo:ITodo, isCompleted:boolean, userId:number) => {
+  try {
+    await axiosInstance.put(`todos/${id}`, {
+      id,
+      todo,
+      isCompleted,
+      userId,
+    },{
+      headers : {
+        Authorization : `Bearer ${token}`
+      }
+    }).then((res)=>console.log(res))
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteTodo = async(id :number)=> {
+  try {
+    await axiosInstance.delete(`todos/${id}`,{
+      headers : {
+        Authorization : `Bearer ${token}`
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
