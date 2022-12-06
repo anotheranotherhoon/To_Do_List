20067c505acfc0ab3f648203158c127bab453e66
LeeChangHoon <changhoon1030@gmail.com>
2022-12-06 21:48:21 +0900

    ⭐ feat : interface 모아서 types파일로 만듬

---
+++ src/type/types.tsx
@@ -0,0 +1,76 @@
+export interface ITodo {
+  id:number;
+  todo : string;
+  isCompleted : boolean;
+  userId : number;
+}
+
+export interface ITodoParam{
+  todo : string
+}
+
+export interface IAuth {
+  email: string;
+  password: string
+}
+
+export interface IAuthProps {
+  isSignInMode : string;
+  handleChangeMode() : void;
+  hadleModalOpen(message:string) : void;
+}
+
+export interface ISignUpAuth {
+  email: string;
+  password: string;
+  password1: string;
+}
+
+export interface IChildren {
+  children : React.ReactNode
+}
+
+export interface IModal {
+  modalClose : ()=>void;
+  todo : string,
+  editToDo(e : React.ChangeEvent<HTMLInputElement>) : void,
+  handleCancelEditMode : ()=>void,
+  isCompleted : boolean,
+  handleComplete : () =>void
+}
+
+export interface IModalAlert {
+  children : string,
+  leftBtnMessage ?: string;
+  rightBtnMessage? : string,
+  leftBtnClick ?: () =>void ,
+  rightBtnClick ?: ()=>void,
+}
+
+export interface IStatus {
+  status : string;
+}
+
+export interface ITheme {
+  theme : string
+}
+
+export interface ILayout {
+  isCompleted : boolean, 
+  isEditMode : boolean
+  theme :ITheme,
+}
+
+export interface IFilter {
+  filterState : string;
+  handleFilter(state : string) : void
+}
+
+export interface IStyledFilter {
+  state : string;
+  current : string
+}
+
+export interface IHeader {
+  handleModalOpen(number : number, message : string) : void;
+}
